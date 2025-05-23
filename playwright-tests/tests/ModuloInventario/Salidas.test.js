cc5e9ae7da3951862a90a325b54622521725b2b2
QA <danq97@gmail.com>
2025-05-23 08:53:43 -0600

    Add: Modulo Inventario Tests

--- playwright-tests/tests/ModuloInventario/Salidas.test.js
+++ playwright-tests/tests/ModuloInventario/Salidas.test.js
@@ -3,15 +3,16 @@ const { busquedaDoc } = require('../helpers/busquedaDoc');
 const credentials = require('../../config/credentials.js');
 const { login } = require('../helpers/login.js');
 
-test.describe('Entradas', () => {
+test.describe('Salidas', () => {
   let page;
   let context;
-  let iframeElement;
+  let iframe;
+  const randomID = `${Date.now()}`.slice(-7);
 
   test.beforeAll(async ({ browser }) => {
     context = await browser.newContext();
     page = await context.newPage();
-    iframeElement = page.frameLocator('iframe');
+    iframe = page.frameLocator('iframe');
 
     // Login
     await test.step('Login', async () => {
@@ -25,7 +26,7 @@ test.describe('Entradas', () => {
     await expect(inventarioBtn).toBeVisible();
     await inventarioBtn.click();
     await page.getByRole('link', { name: 'Salidas', exact: true }).click();
-    iframeElement = page.frameLocator('iframe');
+    iframe = page.frameLocator('iframe');
   });
 
   test.afterAll(async () => {
@@ -33,7 +34,79 @@ test.describe('Entradas', () => {
     await context.close();
   });
 
-  test.fixme('Grabar salidas', async () => {
-    //TODO: Grabar salida de inventario
+  test('Grabar salida de inventario', async () => {
+    const concepto = 'Salida de prueba';
+    await iframe.getByRole('textbox', { name: 'Tipo doc' }).click();
+    await iframe.getByRole('option', { name: /Ajuste de salida/ }).click();
+    await iframe.getByRole('textbox', { name: 'Concepto' }).fill(concepto);
+
+    await iframe.locator('#btnAddLine').click();
+
+    await iframe.getByRole('textbox', { name: 'Almacen' }).click();
+    await iframe.locator('[role="option"][data-index="0"]').click(); // INDEX=0: Almacen 1
+    await iframe.getByRole('textbox', { name: 'Item' }).click();
+    await iframe.locator('[role="option"][data-index="1"]').click();
+    await iframe.getByRole('spinbutton', { name: 'Cantidad' }).fill('10');
+    await iframe.getByRole('spinbutton', { name: 'Costo uni' }).fill('100');
+    await iframe.locator('#btnConfirmAddLine').click();
+    await iframe.getByRole('button', { name: 'Grabar Documento' }).click();
+    await expect(iframe.locator('.mbsc-toast')).toHaveText('Documento ha sido grabado');
+  });
+
+  test.fixme('Modificar salida de inventario', async () => {
+    const concepto = 'Salida modificada: ' + randomID;
+
+    const today = new Date();
+
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    const yyyy = today.getFullYear();
+
+    const TDate = `${yyyy}-${mm}-${dd}`;
+
+
+    await iframe.getByRole('button', { name: 'Buscar documento' }).click();
+    await iframe.getByRole('button', { name: 'Por fecha' }).click();
+
+    await iframe.getByRole('textbox', { name: 'Fecha' }).fill(TDate);
+
+    await iframe.getByRole('textbox', { name: 'Tipo de Salida' }).click();
+    await iframe.getByRole('option', { name: /Ajuste de salida/ }).click();
+    await iframe.getByRole('button', { name: 'Buscar' }).click();
+
+    await iframe.getByRole('row', { name: 'Salida de prueba' }).first().click();
+    await iframe.getByRole('textbox', { name: 'Concepto' }).fill(concepto);
+    await iframe.getByRole('button', { name: 'Grabar cambios' }).click();
+    await expect(iframe.locator('.mbsc-toast')).toHaveText('Cambios han sido grabados');
+  });
+
+  test.fixme('Anular salida de inventario', async () => {
+    const concepto = 'Salida modificada: ' + randomID;
+    const conceptoAnulado = 'Salida anulada: ' + randomID;
+
+    const today = new Date();
+
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    const yyyy = today.getFullYear();
+
+    const TDate = `${yyyy}-${mm}-${dd}`;
+
+    await iframe.getByRole('button', { name: 'Anular documento' }).click();
+    await iframe.getByRole('button', { name: 'Por fecha' }).click();
+
+    await iframe.getByRole('textbox', { name: 'Desde' }).fill(TDate);
+    await iframe.getByRole('textbox', { name: 'Hasta' }).fill(TDate);
+
+    await iframe.getByRole('textbox', { name: 'Tipo de movimiento' }).click();
+    await iframe.getByRole('option', { name: /Ajuste de salida/ }).click();
+    await iframe.getByRole('button', { name: 'Buscar' }).click();
+
+    await iframe.getByRole('row', { name: concepto }).click();
+    await iframe.getByRole('textbox', { name: 'Concepto' }).fill(conceptoAnulado);
+
+    await iframe.locator('#btnConfirmNull').click();
+    await iframe.getByRole('button', { name: 'Si - proceder' }).click();
+    await expect(iframe.locator('.mbsc-toast')).toHaveText('Cambios han sido grabados');
   });
 });
